83a087f47b03ea403b3ea6350d05f38cfeb6ad12
Robell <robellasfaw@gmail.com>
2025-05-01 11:47:13 +0100

    Add title

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <section className="header">
+      <div className="header__container">
+        <h2 className="header__title">TODO LIST</h2>
+      </div>
+    </section>
+  );
+}
