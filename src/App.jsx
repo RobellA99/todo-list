1e84c06587beb77e9db9af5d425cf741ab6f6e4b
Robell <robellasfaw@gmail.com>
2025-05-01 12:33:08 +0100

    Add item cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import ItemCard from "./components/ItemCard/ItemCard";
 
 export default function App() {
   return (
     <>
       <Header />
+      <div className="itemcard">
+        <ItemCard />
+      </div>
     </>
   );
 }
---
+++ src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,38 @@
+import "./ItemCard.scss";
+
+export default function ItemCard() {
+  return (
+    <>
+      <article className="card">
+        <div className="card__container">
+          <h3 className="card__title">Title</h3>
+        </div>
+      </article>
+      <article className="card">
+        <div className="card__container">
+          <h3 className="card__title">Title</h3>
+        </div>
+      </article>
+      <article className="card">
+        <div className="card__container">
+          <h3 className="card__title">Title</h3>
+        </div>
+      </article>
+      <article className="card">
+        <div className="card__container">
+          <h3 className="card__title">Title</h3>
+        </div>
+      </article>
+      <article className="card">
+        <div className="card__container">
+          <h3 className="card__title">Title</h3>
+        </div>
+      </article>
+      <article className="card">
+        <div className="card__container">
+          <h3 className="card__title">Title</h3>
+        </div>
+      </article>
+    </>
+  );
+}
